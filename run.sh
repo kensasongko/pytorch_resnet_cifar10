cecc74b77b0c5965372ff7f847c2e975a025dfee
Ken Sasongko <ken.sasongko@gmail.com>
2019-07-27 01:34:03 +0700

    Changed to train only needed models.

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-for model in resnet20 resnet32 resnet44 resnet56 resnet110 resnet1202
+noise=0.06
+#for model in resnet20 resnet32 resnet44 resnet56 resnet110 resnet1202
+for model in resnet20 resnet110
 do
-    echo "python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model"
-    python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model
+    echo "python -u trainer.py  --arch=$model  --save-dir=save_${model}_$noise |& tee -a log_${model}_$noise"
+    python -u trainer.py --arch=$model --save-dir=save_${model}_$noise |& tee -a log_${model}_$noise
 done
